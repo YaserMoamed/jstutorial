56a4b0b263b1f463134a86f96c94fe9d186d37ee
Yaser Mohamed <37300351+YaserMoamed@users.noreply.github.com>
2020-06-01 11:45:01 +0200

    js1

---
+++ README.md
@@ -0,0 +1,24 @@
+## Javascript
+
+- create html file done
+- create Js file done
+- link Js file with the html file done
+- turn on the live server done
+
+### js simple intro
+
+1995
+
+- what is js done
+- comment done
+- log something in the console done
+- variable done
+- print something in the html doc done
+- data type in js
+  [
+- Number done
+- String done
+- Boolean — true or false done
+- Null — no value
+- Undefined — a declared variable but hasn’t been given a value
+  ]
---
+++ app.js
@@ -0,0 +1,36 @@
+// single
+/*
+multline 
+
+*/
+// echo / cout / print python & dart
+// console.log("Hello world!");
+// var let const
+// var name = "yaser";
+// /*
+// firstName
+// $firstname
+// _firstname
+// */
+// var firstname = "mohamed";
+
+document.getElementById("demo").innerHTML = "We learn JS";
+
+// var x = 8;
+// var y = 889.987;
+// console.log(y);
+
+// var $firstname = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+// document.write($firstname);
+
+// var n = true;
+// var m = false;
+
+// console.log(m);
+// console.log(n);
+
+var b = null;
+console.log(b);
+
+var t;
+console.log(t);
